9ace183e87780258ac90307fc39d78d23ed57f08
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-05-10 19:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (99.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,23 +3,17 @@ public:
     bool isPalindrome(string s) {
         int i=0;
         int j=s.size()-1;
-        if(s.size()==1){
-            return true;
-        }
-
         while(i<=j){
             if(!isalnum(s[i])){
                 i++;
-                continue;
             }
-            if(!isalnum(s[j])){
+            else if(!isalnum(s[j])){
                 j--;
-                continue;
-            }
-            if(tolower(s[i])!=tolower(s[j])){
-                return false;
             }
             else{
+                if(tolower(s[i])!=tolower(s[j])){
+                    return false;
+                }
                 i++;
                 j--;
             }
