7fc151b9bd1d0fc05920be7525304aa9e05d0be3
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-06-04 13:09:12 +0530

    Time: 54 ms (71.78%) | Memory: 76.1 MB (21.18%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
 vector<vector<int>>m;
     int middle(string& s,int i,int j){
-        if(i<0 ||j>=s.size()){
+        if(i>j){
             return 0;
+        }
+        if(s[i]==s[j] && i==j){
+            return 1;
         }
         if(m[i][j]!=-1){
             return m[i][j];
         }
         if(s[i]==s[j] && i!=j){
-            return m[i][j]= 2+middle(s,i-1,j+1);
-        }
-        if(s[i]==s[j] && i==j){
-            return m[i][j]= 1+middle(s,i-1,j+1);
+            return m[i][j]= 2+middle(s,i+1,j-1);
         }
         else{
-            return m[i][j]= max(middle(s,i,j+1),middle(s,i-1,j));
+            return m[i][j]= max(middle(s,i+1,j),middle(s,i,j-1));
         }
         return m[i][j];
     }
@@ -23,10 +23,7 @@ vector<vector<int>>m;
         //subsequence
         int ans=0;
         m=vector<vector<int>>(s.size()+1,vector<int>(s.size()+1,-1));
-        for(int i=0;i<s.size();i++){
-            ans=max(ans,middle(s,i,i+1));
-            ans=max(ans,middle(s,i,i));
-        }
+        return middle(s,0,s.size()-1);
         return ans;
     }
 };
