778d82b54480300b435cb50a74989dc780a25210
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-06-12 15:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.00%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,18 +1,53 @@
+class TrieNode{
+    public:
+    unordered_map<char,TrieNode*>children;
+    bool endWord;
+};
+class Trie{
+    public:
+    TrieNode*root;
+    Trie(){
+        root=new TrieNode();
+    }
+    void insert(string word){
+        TrieNode*node=root;
+        for(auto i:word){
+            if(node->children.find(i)==node->children.end()){
+                node->children[i]=new TrieNode();
+            }
+            node=node->children[i];
+        }
+        node->endWord=true;
+
+    }
+    int comm(string word){
+        TrieNode*node=root;
+        for(int i=0;i<word.size();i++){
+            if(node->children.find(word[i])==node->children.end()){
+                return i;
+            }
+            node=node->children[word[i]];
+        }
+        return word.size();
+    }
+};
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(),strs.end());
-        string l=strs[0];
-        string h=strs[strs.size()-1];
-        string ans;
-        for(int i=0;i<(l.size(),h.size());i++){
-            if(l[i]==h[i]){
-                ans=ans+l[i];
-            }
-            else{
-                break;
+        int minl=1e9;
+        int idx=0;
+        for(int i=0;i<strs.size();i++){
+            if(minl>strs[i].size()){
+                minl=strs[i].size();
+                idx=i;
             }
         }
-        return ans;
+        Trie trie;
+        int l=1e9;
+        trie.insert(strs[idx]);
+        for(auto i:strs){
+             l=min(l,trie.comm(i.substr(0,minl)));
+        }
+        return strs[idx].substr(0,l);
     }
 };
