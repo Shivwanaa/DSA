1b939406b69007c8b93402f813b351ae258ff7dc
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-06-23 23:42:30 +0530

    Time: 59 ms (14.31%) | Memory: 53.8 MB (8.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int check(vector<int>& coins, int amount,int i,vector<vector<int>>&m){
+vector<vector<int>>dp;
+    int check(vector<int>& coins, int amount,int i){
         if(amount==0){
             return 0;
         }
-        if(i==coins.size() || amount<0){
+        if(i==coins.size()||amount<0){
             return 1e9;
         }
-        if(m[i][amount]!=-1){
-            return m[i][amount];
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
         }
-        int p=1+check(coins,amount-coins[i],i,m);
-        int np=check(coins,amount,i+1,m);
-        m[i][amount]=min(p,np);
-        return m[i][amount];
+        int a=1+check(coins,amount-coins[i],i);
+        int b=check(coins,amount,i+1);
+        return dp[i][amount]= min(a,b);
     }
     int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(),coins.end());
-        vector<vector<int>>m(coins.size()+1,vector<int>(amount+1,-1));
-        int ans=check(coins,amount,0,m);
+        dp=vector<vector<int>>(coins.size()+1,vector<int>(amount+1,-1));
+        int ans=check(coins,amount,0);
         if(ans==1e9){
             return -1;
         }
