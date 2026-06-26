99693a6dafaab483b6518b58aee75a1cba18603f
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-06-27 00:09:46 +0530

    Time: 31 ms (95.48%) | Memory: 14.6 MB (66.20%) - LeetSync

--- 546-remove-boxes/remove-boxes.cpp
+++ 546-remove-boxes/remove-boxes.cpp
@@ -5,26 +5,26 @@ int dp[101][101][101];
         if(j-i<0){
             return 0;
         }
-        int counthere=count;
-        int ihere=i;
         if(dp[i][j][count]!=-1){
             return dp[i][j][count];
         }
+        int ii=i;
+        int jj=j;
+        int count1=count;
         while(i+1<j && boxes[i]==boxes[i+1]){
             i++;
             count++;
         }
-        int ans=(count+1)*(count+1)+check(boxes,i+1,j,0);
-        for(int idx=i+1;idx<=j;idx++){
-            if(boxes[i]==boxes[idx]){
-                ans=max(ans,check(boxes,i+1,idx-1,0)+check(boxes,idx,j,count+1));
+        int best=(count+1)*(count+1)+check(boxes,i+1,j,0);
+        for(int k=i+1;k<=j;k++){
+            if(boxes[k]==boxes[i]){
+                best=max(best,check(boxes,i+1,k-1,0)+check(boxes,k,j,count+1));
             }
         }
-        return dp[ihere][j][counthere]=ans;
+        return dp[ii][j][count1]=best;
     }
     int removeBoxes(vector<int>& boxes) {
         memset(dp,-1,sizeof(dp)); 
         return check(boxes,0,boxes.size()-1,0);
     }
 };
-
