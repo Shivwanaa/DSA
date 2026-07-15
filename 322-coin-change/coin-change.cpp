79db055682e0340523595cd313a1d0f6defba327
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-07-15 10:51:11 +0530

    Time: 59 ms (14.43%) | Memory: 53.9 MB (7.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
 vector<vector<int>>dp;
-    int check(vector<int>& coins, int amount,int i){
+    int check(int i,vector<int>& coins, int amount){
         if(amount==0){
             return 0;
         }
-        if(i==coins.size()||amount<0){
+        if(amount<0||i>=coins.size()){
             return 1e9;
         }
         if(dp[i][amount]!=-1){
             return dp[i][amount];
         }
-        int a=1+check(coins,amount-coins[i],i);
-        int b=check(coins,amount,i+1);
-        return dp[i][amount]= min(a,b);
+        int p=1+check(i,coins,amount-coins[i]);
+        int np=check(i+1,coins,amount);
+        return dp[i][amount]=min(p,np);
     }
     int coinChange(vector<int>& coins, int amount) {
         dp=vector<vector<int>>(coins.size()+1,vector<int>(amount+1,-1));
-        int ans=check(coins,amount,0);
+        int ans= check(0,coins,amount);
         if(ans==1e9){
             return -1;
         }
