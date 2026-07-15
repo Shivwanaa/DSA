893f9adb239a2b65a7dcfccc39d85ff4d04a9ac1
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-07-15 19:32:28 +0530

    Time: 639 ms (5.10%) | Memory: 296.7 MB (6.60%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
 vector<vector<int>>dp;
-    int check(vector<int>& nums,int i,int prev){
+    int check(int i,int j,vector<int>&nums){
         if(i>=nums.size()){
             return 0;
         }
-        if(dp[i][prev+1]!=-1){
-            return dp[i][prev+1];
+        if(dp[i][j+1]!=-1){
+            return dp[i][j+1];
         }
-        int a=0,b=0,c=0;
-        if(prev==-1||nums[prev]<nums[i]){
-            a=1+check(nums,i+1,i);
-        }
-        c=check(nums,i+1,prev);
-        dp[i][prev+1]= max(a,c);
-        return dp[i][prev+1];
+        int p=0,np=0;
+        if(j==-1 || nums[i]>nums[j])
+        p=1+check(i+1,i,nums);
+        np=check(i+1,j,nums);
+        return dp[i][j+1]= max(p,np);
     }
     int lengthOfLIS(vector<int>& nums) {
         dp=vector<vector<int>>(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return check(nums,0,-1);
+        return check(0,-1,nums);
     }
 };
