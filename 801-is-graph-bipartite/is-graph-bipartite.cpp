76ddafd79fd58eed3350e70d24c66a944f6b1487
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-07-17 14:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (26.66%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,41 +1,26 @@
-class DSU{
-    public:
-    vector<int>p;
-    vector<int>s;
-    DSU(int n){
-        p.resize(n);
-        s.resize(n,1);
-        for(int i=0;i<n;i++){
-            p[i]=i;
-        }
-    }
-    int find(int node){
-        if(p[node]!=node){
-            return find(p[node]);
-        }
-        return p[node];
-    }
-    bool Union(int u,int v){
-        int pu=find(u);
-        int pv=find(v);
-        if(pu==pv){
-            return false;
-        }
-        p[pu]=pv;
-        s[pv]=s[pv]+s[pu];
-        return true;
-    }
-};
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        DSU dsu(graph.size());
-        for(int node=0;node<graph.size();node++){
-            for(auto i:graph[node]){
-                if(dsu.find(i)==dsu.find(node)){
-                    return false;
+        vector<int>c(graph.size(),0);
+        for(int i=0;i<graph.size();i++){
+            if(c[i]!=0){
+                continue;
+            }
+            queue<int>q;
+            c[i]=-1;
+            q.push(i);
+            while(q.size()){
+                int t=q.front();
+                q.pop();
+                for(auto i:graph[t]){
+                    if(c[i]==c[t]){
+                        return false;
+                    }
+                    else if(c[i]==0){
+                        q.push(i);
+                        c[i]=-c[t];
+                    }
                 }
-                dsu.Union(graph[node][0],i);
             }
         }
         return true;
