31efbfa072cd12c5b215e42e741f537d8cf64882
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-07-18 18:56:13 +0530

    Time: 3 ms (65.49%) | Memory: 11 MB (17.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()){
-            return false;
-        }
         unordered_map<char,int>m;
-        unordered_map<char,int>mtemp;
         for(auto i:s1){
             m[i]++;
         }
+        unordered_map<char,int>mm;
+        unordered_map<char,int>match;
         int i=0;
         for(int j=0;j<s2.size();j++){
-            mtemp[s2[j]]++;
-            if(mtemp==m){
-                return true;
-            }
+            match[s2[j]]++;
             if(j-i+1==s1.size()){
-                mtemp[s2[i]]--;
-                if(mtemp[s2[i]]==0){
-                    mtemp.erase(s2[i]);
+                if(m==match)
+                return true;
+                match[s2[i]]--;
+                if(match[s2[i]]==0){
+                    match.erase(s2[i]);
                 }
                 i++;
             }
