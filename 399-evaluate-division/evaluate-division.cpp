7273f17861ffd51345ae7a55f00d15a791192775
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-07-24 07:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.54%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
-    double check(string n1,string n2,map<pair<string,string>,double>&m,unordered_map<string,vector<string>>&g,unordered_set<string>&v,double a){
-        if(g.find(n1)==g.end() || g.find(n2)==g.end()){
-            return -1;
+    double check(string a,string b,map<pair<string,string>,double>&m,unordered_map<string,vector<pair<string,double>>>&dfs,double temp,unordered_set<string>&v){
+        if(dfs.find(a)==dfs.end() ||dfs.find(b)==dfs.end()){
+            return -1.0;
         }
-        if(n1==n2){
-            return a;
+        if(a==b){
+            return temp;
         }
-        v.insert(n1);
-        for(auto i:g[n1]){
-            if(v.find(i)==v.end()){
-            double ans=check(i,n2,m,g,v,a*m[{n1,i}]);
-            if(ans!=-1){
-            return ans;
+        v.insert(a);
+        for(auto i:dfs[a]){
+            if(!v.count(i.first)) {
+            double ans = check(
+                    i.first,
+                    b,
+                    m,
+                    dfs,
+                    temp * i.second,
+                    v
+                );
+
+                if(ans != -1.0) {
+                    return ans;
+                }
         }
         }
-        }
-    v.erase(n1);
-    return -1;
+        v.erase(a);
+        return -1.0;
     }
+
+
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        map<pair<string,string>,double>m;
-        int idx=0;
-        unordered_map<string,vector<string>>g;
-        for(auto i:equations){
-            g[i[0]].push_back(i[1]);
-            g[i[1]].push_back(i[0]);
-            m[{i[0],i[1]}]=double(values[idx]);
-            m[{i[1],i[0]}]=double(1.0/double(values[idx]));
-            idx++;
-        }
         vector<double>ans;
-        
+        map<pair<string,string>,double>m;
+        int j=0;
+        unordered_map<string,vector<pair<string,double>>>dfs;
+        for(auto i:equations){
+            m[{i[0],i[1]}]=(values[j]);
+            m[{i[1],i[0]}]=(double(1/values[j]));
+            dfs[i[0]].push_back({i[1],values[j]});
+            dfs[i[1]].push_back({i[0],double(1.0/values[j])});
+            j++;
+        }
         for(auto i:queries){
             unordered_set<string>v;
-            ans.push_back(check(i[0],i[1],m,g,v,1.0));
-            
+            ans.push_back(check(i[0],i[1],m,dfs,1.0,v));
         }
         return ans;
-}
+    }
 };
