179e987bccc97d09d5e656cc193a33a39a11bcd8
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-01 20:39:10 +0530

    Time: 64 ms (6.00%) | Memory: 34.2 MB (5.29%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
 vector<vector<string>>ans;
-//place q in 1 position
-//relative to that place the next q in a placee where its not attacking and then the next queen,-recursive manner
-    void check(int i,int n,vector<string>t,unordered_set<int>c,unordered_set<int>nd,unordered_set<int>pd){
+    void check(int i,vector<string>t,int n,unordered_set<int>c,unordered_set<int>pd,unordered_set<int>nd){
         if(i==n){
             ans.push_back(t);
             return;
         }
-        for(int j=0;j<n;j++){
-            if(!c.count(j) && !nd.count(i-j) && !pd.count(i+j)){
-                c.insert(j);
-                nd.insert(i-j);
-                pd.insert(i+j);
-                t[i][j]='Q';
-                check(i+1,n,t,c,nd,pd);
-                t[i][j]='.';
-                c.erase(j);
-                nd.erase(i-j);
-                pd.erase(i+j);
-            }
-            else{
+        for(int k=0;k<n;k++){
+            if(c.count(k) || pd.count(i+k)|| nd.count(i-k)){
                 continue;
             }
+            pd.insert(i+k);
+            nd.insert(i-k);
+            c.insert(k);
+            t[i][k]='Q';
+            check(i+1,t,n,c,pd,nd);
+            t[i][k]='.';
+            pd.erase(i+k);
+            nd.erase(i-k);
+            c.erase(k);
         }
+
     }
     vector<vector<string>> solveNQueens(int n) {
+        unordered_set<int>c;
+        unordered_set<int>pd;
+        unordered_set<int>nd;
         vector<string>t(n,string(n,'.'));
-        unordered_set<int>c,nd,pd;
-        check(0,n,t,c,nd,pd);
+        check(0,t,n,c,pd,nd);
         return ans;
     }
 };
