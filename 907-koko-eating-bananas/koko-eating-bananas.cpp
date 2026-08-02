94665f8efc12a16f56a2fb6a98c6ea498cf75d12
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-02 06:55:33 +0530

    Time: 12 ms (26.19%) | Memory: 22.9 MB (53.14%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    long long check(int mid,vector<int>&nums){
-        long long  a=0;
-        for(auto i:nums){
-            a=a+ceil(double(i)/double(mid));
+    long long geth(int mid,vector<int>&piles){
+        long long s=0;
+        for(int i=0;i<piles.size();i++){
+            s += (1LL * piles[i] + mid - 1) / mid;
         }
-        return a;
+        return s;
     }
-    int minEatingSpeed(vector<int>& nums, int h) {
-        int l=1;
-        int r=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            r=max(r,nums[i]);
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int m=0;
+        for(int i=0;i<piles.size();i++){
+            m=max(m,piles[i]);
         }
-        int ans=0;
+        int l=1;
+        int r=m;
+        int ans;
         while(l<=r){
             int mid=(l+r)/2;
-            cout<<mid<< " "<<check(mid,nums)<<endl;
-            if(check(mid,nums)>h){
-                l=mid+1;
+            cout<<mid;
+            if(geth(mid,piles)<=h){
+                ans=mid;
+                r=mid-1;
+                
             }
             else{
-               r=mid-1;
+                l=mid+1;
             }
         }
-        return l;
+        return ans;
     }
 };
