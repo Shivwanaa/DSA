74fa11df7d0114bed15b98dafb29b40315463c90
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-07 17:04:28 +0530

    Time: 21 ms (94.44%) | Memory: 40.7 MB (32.71%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -3,53 +3,60 @@ class DSU{
     vector<int>p;
     vector<int>s;
     DSU(int n){
-        p.resize(n);
-        s.resize(n);
-        for(int i=0;i<n;i++){
+        p.resize(n+1);
+        s.resize(n+1);
+        for(int i=0;i<n+1;i++){
             p[i]=i;
             s[i]=1;
         }
     }
+
     int find(int node){
         if(p[node]!=node){
-            return find(p[node]);
+           p[node] = find(p[node]); 
         }
         return p[node];
     }
-    bool Union(int x,int y){
-        int px=find(x);
-        int py=find(y);
-        if(px==py) return false;
-        p[px]=py;
-        s[px]=s[py]+s[px];
+    bool Union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv){
+            return false;
+        }
+        p[pu]=pv;
+        s[pv]=s[pv]+s[pu];
         return true;
     }
 };
 class Solution {
 public:
+
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        vector<vector<string>>ans;
-        unordered_map<string,int>m;
         DSU dsu(accounts.size());
+        unordered_map<string,int>v;
+        unordered_map<int,vector<string>>m;
         for(int i=0;i<accounts.size();i++){
             for(int j=1;j<accounts[i].size();j++){
-                if(m.find(accounts[i][j])!=m.end()){
-                    dsu.Union(m[accounts[i][j]],i);
+                if(v.count(accounts[i][j])){
+                    dsu.Union(v[accounts[i][j]],i);
                 }
                 else{
-                m[accounts[i][j]]=i;
+                v[accounts[i][j]]=i;
                 }
             }
         }
-        map<int,vector<string>>temp;
-        for(auto i:m){
-            temp[dsu.find(i.second)].push_back(i.first);
+        for(auto i:v){
+            m[dsu.find(i.second)].push_back(i.first);
         }
-        for(auto i:temp){
-            // vector<string>t;
+        vector<vector<string>>ans;
+        for(auto i:m){
+            
             sort(i.second.begin(),i.second.end());
-            i.second.insert(i.second.begin(),accounts[i.first][0]);
-            ans.push_back(i.second);
+            vector<string>t;
+            t=i.second;
+            t.insert(t.begin(),accounts[i.first][0]);
+            ans.push_back(t);
+
         }
         return ans;
     }
