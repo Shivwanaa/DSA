f0745ef09e175629ab3d831843c5a67aa748a3f9
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-07 15:45:33 +0530

    Time: 3 ms (32.77%) | Memory: 13.4 MB (44.34%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,44 +1,43 @@
 class DSU{
+    public:
     vector<int>p;
     vector<int>s;
-    public:
     DSU(int n){
         p.resize(n+1);
         s.resize(n+1);
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<n+1;i++){
             p[i]=i;
             s[i]=1;
         }
     }
+
     int find(int node){
         if(p[node]!=node){
             return find(p[node]);
         }
-        return node;
+        return p[node];
     }
     bool Union(int u,int v){
         int pu=find(u);
         int pv=find(v);
-        if(pu!=pv){
-            p[pu]=pv;
-            s[pv]=s[pv]+s[pu];
-            return true;
+        if(pu==pv){
+            return false;
         }
-        return false;
+        p[pu]=pv;
+        s[pv]=s[pv]+s[pu];
+        return true;
     }
-
 };
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int>ans;
         DSU dsu(edges.size());
         for(auto i:edges){
             if(dsu.find(i[0])==dsu.find(i[1])){
-                return i;
+                return {i[0],i[1]};
             }
             dsu.Union(i[0],i[1]);
         }
-        return ans;
+        return {};
     }
 };
