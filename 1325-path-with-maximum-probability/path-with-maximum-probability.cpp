6346fe5f81c43f8d44d36a55cc2bb6d75d73a21f
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-08 20:02:57 +0530

    Time: 77 ms (10.28%) | Memory: 75 MB (11.70%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -2,33 +2,32 @@ class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
         unordered_map<int,vector<pair<int,double>>>m;
-        int j=0;
-        for(auto i:edges){
-            m[i[0]].push_back({i[1],succProb[j]});
-            m[i[1]].push_back({i[0],succProb[j]});
-            j++;
+        for(int i=0;i<edges.size();i++){
+            m[edges[i][0]].push_back({edges[i][1],double(succProb[i])});
+            m[edges[i][1]].push_back({edges[i][0],double(succProb[i])});
         }
-        auto comp=[](pair<int,double>a,pair<int,double>b){
-            return a.second<b.second;
-        };
-        priority_queue<pair<int,double>,vector<pair<int,double>>,decltype(comp)>q(comp);
-        q.push({start_node,1});
-        unordered_set<int>v;
-        while(q.size()){
-            auto[node,prob]=q.top();
-            q.pop();
+        priority_queue<pair<double,int>>pq;
+        pq.push({1.0,start_node});
+        int ans=0;
+        vector<int>vist(n,0);
+        vector<double>probs(n,-1);
+        probs[start_node]=1.0;
+        while(pq.size()){
+            auto[prob,node]=pq.top();
+            pq.pop();
+            if(prob<probs[node]){
+                continue;
+            }
             if(node==end_node){
                 return prob;
             }
-            if(v.count(node)){
-                continue;
-            }
-            v.insert(node);
             for(auto i:m[node]){
-                // cout<<i.second<<endl;
-                q.push({i.first,i.second*prob});
+                if(probs[i.first]<prob*i.second){
+                probs[i.first]=prob*i.second;
+                pq.push({prob*i.second,i.first});
+                }
             }
         }
-        return 0;
+        return 0.0;
     }
 };
