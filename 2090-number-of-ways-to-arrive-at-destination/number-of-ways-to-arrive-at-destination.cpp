dd0d61fd117958dfe31857998682a7aac4faa362
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-08 19:10:49 +0530

    Time: 18 ms (15.33%) | Memory: 37.4 MB (31.82%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
+const int MOD=1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>>m(n);
-        const int MOD = 1e9 + 7;
+        unordered_map<int,vector<pair<int,int>>>m;
         for(auto i:roads){
             m[i[0]].push_back({i[1],i[2]});
             m[i[1]].push_back({i[0],i[2]});
         }
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<>>q;
-        int ans=0;
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>q;
         q.push({0,0});
-        vector<long long>dist(n,LLONG_MAX);
-        vector<int>c(n,0);
-        dist[0]=0;
+        vector<long long>noded(n,1e18);
+        noded[0]=0;
+        vector<long long>c(n,0);
         c[0]=1;
         while(q.size()){
-            auto [t,f]=q.top();
+            auto [dist,top]=q.top();
             q.pop();
-            if(dist[f]<t){
-                continue;
-            }
-            for(auto i:m[f]){
-                if(dist[i.first]>t+i.second){
-                    dist[i.first]=t+i.second;
-                    c[i.first]=c[f];
-                   q.push({i.second+t,i.first});
+            if(dist != noded[top])
+        continue;
+            for(auto i:m[top]){
+                if(noded[i.first]>dist+i.second){
+                    noded[i.first]=dist+i.second;
+                    c[i.first]=c[top];
+                    q.push({dist+i.second,i.first});
                 }
-                else if(t+i.second==dist[i.first]){
-                    c[i.first]=(c[i.first]+c[f])%MOD;
+                else if(dist + i.second == noded[i.first]){
+                        c[i.first]=(c[i.first]+c[top])% (1000000007);
+                    
                 }
             }
         }
