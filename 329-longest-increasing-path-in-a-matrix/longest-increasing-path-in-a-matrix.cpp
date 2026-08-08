a987efcab4129fe8c7d9e8a945b2128458532136
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-08 11:11:41 +0530

    Time: 7 ms (81.45%) | Memory: 20.7 MB (86.29%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+int dir[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
+vector<vector<int>>dp;
+    int check(int i,int j,vector<vector<int>>& matrix){
+        int ans=1;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        for(auto k:dir){
+            int ni=i+k[0],nj=j+k[1];
+            if(ni<0||nj<0||ni>=matrix.size()||nj>=matrix[0].size()){
+                continue;
+            }
+            if(matrix[i][j]<matrix[ni][nj])
+            ans=max(ans,1+check(ni,nj,matrix));
+        }
+        // matrix[i][j]=t;
+        return dp[i][j]=ans;
+
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int ans=0;
+        dp=vector<vector<int>>(matrix.size(),vector<int>(matrix[0].size(),-1));
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                ans=max(ans,check(i,j,matrix));
+            }
+        }
+        return ans;
+    }
+};
