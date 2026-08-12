aefd355db07d99e3a20ca0ac25bc29e42b9a164d
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-13 00:54:10 +0530

    Time: 138 ms (6.39%) | Memory: 26.7 MB (10.11%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        //start with nodes having least no of connections and continue in the same way
-        set<pair<int,int>>v;
         queue<pair<int,int>>q;
         for(int i=0;i<graph.size();i++){
-            q.push({i,1<<i});
-            v.insert({i,1<<i});
+            int m=1<<i;
+            q.push({i,m});
         }
+        set<pair<int,int>>v;
+        int bit_act=(1<<graph.size())-1;
         int ans=0;
+
         while(q.size()){
-            int l=q.size();
-            for(int i=0;i<l;i++){
+            int s=q.size();
+            for(int i=0;i<s;i++){
                 auto[node,mask]=q.front();
                 q.pop();
-                if(mask==(1<<graph.size())-1){
+                if(v.count({node,mask})){
+                    continue;
+                }
+                v.insert({node,mask});
+                if(mask==bit_act){
                     return ans;
                 }
                 for(auto k:graph[node]){
-                    int t=(mask|(1<<k));
-                    if(v.count({k,t})){
+                    int bit_temp=(1<<k)|mask;
+                    if(v.count({k,mask})){
                         continue;
                     }
-                    v.insert({k,t});
-                    q.push({k,t});
+                    q.push({k,bit_temp});
                 }
             }
             ans++;
         }
-        return -1;
+        return ans;
     }
 };
