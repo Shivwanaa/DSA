93f290541017fc8e0855101abdee73207eff1bdb
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-14 12:50:55 +0530

    Time: 35 ms (21.29%) | Memory: 74.7 MB (11.36%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -3,16 +3,12 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         unordered_map<int,int>in;
         unordered_map<int,int>out;
-        for(int i=1;i<=n;i++){
-            in[i]=0;
-            out[i]=0;
-        }
         for(auto i:trust){
-            in[i[1]]++;
             out[i[0]]++;
+            in[i[1]]++;
         }
         for(int i=1;i<=n;i++){
-            if(out[i]==0 && in[i]==n-1){
+            if(in[i]==n-1 && out[i]==0){
                 return i;
             }
         }
