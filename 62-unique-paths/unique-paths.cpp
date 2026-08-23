9bc49185668b52e5a42391cc86dc01f939f81ab3
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-23 23:12:12 +0530

    Time: 3 ms (9.40%) | Memory: 9.4 MB (31.12%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-vector<vector<int>>mp;
-    int dir[2][2]={{1,0},{0,1}};
+vector<vector<int>>dp;
     int check(int i,int j,int m,int n){
         if(i==m-1 && j==n-1){
             return 1;
         }
-        if(i>=m ||j>=n||i<0||j<0){
+        if(i<0||j<0||i>=m||j>=n){
             return 0;
         }
-        if(mp[i][j]!=-1){
-            return mp[i][j];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int ans=0;
-        for(auto k:dir){
-            ans=ans+check(i+k[0],j+k[1],m,n);
-        }
-        
-        return mp[i][j]=ans;
+        int d=check(i+1,j,m,n);
+        int u=check(i,j+1,m,n);
+        return dp[i][j]= d+u;
     }
     int uniquePaths(int m, int n) {
-        mp=vector<vector<int>>(m,vector<int>(n,-1));
+        dp=vector<vector<int>>(m,vector<int>(n,-1));
         return check(0,0,m,n);
     }
 };
