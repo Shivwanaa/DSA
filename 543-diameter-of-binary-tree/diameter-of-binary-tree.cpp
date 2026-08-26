eaf4d75025d8a4ed59e10ebb75060486140de772
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-26 22:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (94.11%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -20,11 +20,10 @@ int ans=0;
         int r=check(root->right);
         ans=max(ans,l+r);
         return 1+max(l,r);
-
     }
     int diameterOfBinaryTree(TreeNode* root) {
         if(!root){
-            return 0;
+            return -1;
         }
         check(root);
         return ans;
