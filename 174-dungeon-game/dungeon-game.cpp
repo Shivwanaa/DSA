af0394e581790db4ec4e07cf53b6a88f4859c85e
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-29 19:25:54 +0530

    Time: 4 ms (11.25%) | Memory: 13.1 MB (52.09%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int rows=dungeon.size();
-        int cols=dungeon[0].size();
-        vector<vector<int>>dp(rows,vector<int>(cols,INT_MAX));
-        auto getmin=[&](int curr,int nr,int nc)->int{
-            if(nr>=rows ||nc>=cols){
-                return INT_MAX;
-            }
-            int nextcell=dp[nr][nc];
-            return max(1,nextcell-curr);
-        };
-        for(int i=rows-1;i>=0;i--){
-            for(int j=cols-1;j>=0;j--){
-                int curr=dungeon[i][j];
-                int rhealth=getmin(curr,i,j+1);
-                int dhealth=getmin(curr,i+1,j);
-                int next=min(rhealth,dhealth);
-                int min_h;
-                if(next!=INT_MAX){
-                    min_h=next;
-                }
-                else{
-                    min_h=curr>=0?1:1-curr;
-                }
-                dp[i][j]=min_h;
+        int m=dungeon.size();
+        int n=dungeon[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,1e9));
+        dp[m-1][n]=1;
+        dp[m][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int n=min(dp[i+1][j],dp[i][j+1])-dungeon[i][j];
+                dp[i][j]=max(1,n);
             }
         }
         return dp[0][0];
