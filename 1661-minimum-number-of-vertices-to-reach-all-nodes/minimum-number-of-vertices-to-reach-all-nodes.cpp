342c51b55f973b29fc37ebf83d4e6ab5a9207cd4
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-30 14:43:09 +0530

    Time: 79 ms (12.74%) | Memory: 122.6 MB (11.56%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        //those that dont have any indegree is the answer
-        vector<int>ans;
         unordered_map<int,int>in;
         for(int i=0;i<n;i++){
             in[i]=0;
@@ -10,6 +8,7 @@ public:
         for(auto i:edges){
             in[i[1]]++;
         }
+        vector<int>ans;
         for(auto i:in){
             if(i.second==0){
                 ans.push_back(i.first);
