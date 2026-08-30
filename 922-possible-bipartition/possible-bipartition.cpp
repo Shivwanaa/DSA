c04653a2f3d60080fa7e9cafc30aaf50cb2bacd0
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-30 12:40:46 +0530

    Time: 341 ms (5.03%) | Memory: 80.3 MB (5.36%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,33 +1,48 @@
+class DSU{
+    public:
+    vector<int>p;
+    int n;
+    DSU(int n){
+        p.resize(n+1);
+        for(int i=0;i<=n;i++){
+            p[i]=i;
+        }
+    }
+    int find(int node){
+        if(p[node]!=node){
+            return find(p[node]);
+        }
+        return p[node];
+    }
+    bool Union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv){
+            return false;
+        }
+        p[pu]=pv;
+        return true;
+    }
+};
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        DSU dsu(n);
         unordered_map<int,vector<int>>m;
         for(auto i:dislikes){
             m[i[0]].push_back(i[1]);
             m[i[1]].push_back(i[0]);
         }
-        vector<int>c(n+1,0);
-        for(int i=1;i<=n;i++){
-            if(c[i]!=0){
-                continue;
-            }
-            c[i]=-1;
-            queue<int>q;
-            q.push(i);
-            while(q.size()){
-                int f=q.front();
-                q.pop();
-                for(auto j:m[f]){
-                    if(c[f]==c[j]){
+        for(auto i:m){
+            if(i.second.size()){
+                int fd=i.second[0];
+                for(auto j:i.second){
+                    if(dsu.find(j)==dsu.find(i.first)){
                         return false;
                     }
-                    else if(c[j]==0){
-                        c[j]=-c[f];
-                        q.push(j);
-                    }
+                    dsu.Union(fd,j);
                 }
             }
-            
         }
         return true;
     }
