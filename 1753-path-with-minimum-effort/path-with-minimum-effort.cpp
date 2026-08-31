24b1166c20ef710a7bf3d31eb40b33786632ef08
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-31 23:35:23 +0530

    Time: 65 ms (36.16%) | Memory: 25.7 MB (33.30%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>>q;
-        vector<vector<int>>effort(heights.size(),vector<int>(heights[0].size(),1e9));
+        //djikistra
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>q;
+        q.push({0,0,0});
         int dir[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
-        effort[0][0]=0;
-        q.push({0,{0,0}});
+        vector<vector<int>> dist(
+            heights.size()+1,
+            vector<int>(heights[0].size()+1, 1e9)
+        );
+        dist[0][0]=0;
+        int temp=1e9;
         while(q.size()){
-            int ef=q.top().first;
-            auto[i,j]=q.top().second;
+            auto[dis,r,c]=q.top();
             q.pop();
-            if(i==heights.size()-1 && j==heights[0].size()-1){
-                return effort[heights.size()-1][heights[0].size()-1];
+            if(dist[r][c]>dis){
+                continue;
             }
-            for(auto k:dir){
-                if(i+k[0]<0||i+k[0]>=heights.size()||j+k[1]<0||j+k[1]>=heights[0].size()){
-                    continue;
-                }
-                int newEffort = max(
-                    effort[i][j],
-                    abs(heights[i+k[0]][j+k[1]]-heights[i][j])
-                );
-                if(newEffort<effort[i+k[0]][j+k[1]]){
-                    effort[i+k[0]][j+k[1]]=newEffort;
-                    q.push({newEffort,{i+k[0],j+k[1]}});
+            if(r==heights.size()-1 && c==heights[0].size()-1){
+                return dis;
+            }
+            for(auto i:dir){
+                int nr=i[0]+r;
+                int nc=i[1]+c;
+                if(nr>=0&& nr<heights.size()&& nc>=0 && nc<heights[0].size()){
+                    temp=max(dis,abs(heights[nr][nc]-heights[r][c]));
+                    if(dist[nr][nc]>temp){
+                        dist[nr][nc]=temp;
+                        q.push({temp,nr,nc});
+                    }
                 }
             }
         }
