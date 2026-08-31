8176f86f9c805fc1f7168850a2ee9e0ed98569d0
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-08-31 21:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (97.47%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-    void check(int i,int j,vector<vector<char>>& grid){
-        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]=='X'||grid[i][j]=='c'){
-            return;
-        }
-        grid[i][j]='c';
-        check(i+1,j,grid);
-        check(i-1,j,grid);
-        check(i,j+1,grid);
-        check(i,j-1,grid);
+   void check(int i,int j,vector<vector<char>>& board){
+    if(i<0||j<0||i>=board.size()||j>=board[0].size()||board[i][j]!='O'){
+        return;
     }
-    void solve(vector<vector<char>>& grid) {
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if((i==0||j==0||i==grid.size()-1||j==grid[0].size()-1)&&grid[i][j]=='O'){
-                    check(i,j,grid);
+    board[i][j]='1';
+    check(i+1,j,board);
+    check(i-1,j,board);
+    check(i,j+1,board);
+    check(i,j-1,board);
+   }
+    void solve(vector<vector<char>>& board) {
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(i==0||j==0||i==board.size()-1||j==board[0].size()-1){
+                    if(board[i][j]=='O'){
+                        check(i,j,board);
+                    }
                 }
             }
         }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]!='c'){
-                    grid[i][j]='X';
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
                 }
-                if(grid[i][j]=='c'){
-                    grid[i][j]='O';
+            }
+        }
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='1'){
+                    board[i][j]='O';
                 }
             }
         }
