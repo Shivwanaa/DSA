8f7cb68f4692d1e4e2cb232f029a56e00295649b
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-09-01 01:10:02 +0530

    Time: 16 ms (35.47%) | Memory: 20.9 MB (50.12%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
 int dir[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
-vector<vector<int>>dp;
-const int MOD = 1e9 + 7;
+vector<vector<int>>dp;;
     int check(int i,int j,vector<vector<int>>& matrix){
-        int ans=1;
+        if(i<0||j<0||i>=matrix.size()||j>=matrix[0].size()){
+            return 0;
+        }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
+        int ans=0;
         for(auto k:dir){
-            int ni=i+k[0],nj=j+k[1];
-            if(ni<0||nj<0||ni>=matrix.size()||nj>=matrix[0].size()){
-                continue;
+            int nr=k[0]+i;
+            int nc=k[1]+j;
+            if(nr>=0 && nr<matrix.size()&& nc>=0 && nc<matrix[0].size()){
+                if(matrix[i][j]<matrix[nr][nc]){
+                    ans=max(ans,1+check(nr,nc,matrix));
+                }
             }
-            if(matrix[i][j]<matrix[ni][nj])
-            ans=max(ans,1+check(ni,nj,matrix));
         }
-        // matrix[i][j]=t;
         return dp[i][j]=ans;
-
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int ans=0;
@@ -28,6 +29,6 @@ const int MOD = 1e9 + 7;
                 ans=max(ans,check(i,j,matrix));
             }
         }
-        return ans;
+        return ans+1;
     }
 };
