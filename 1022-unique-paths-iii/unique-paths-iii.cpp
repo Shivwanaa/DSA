6f4ad16ae6d2ff5b40b1cf0dc5392e46d8895226
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-09-05 14:47:39 +0530

    Time: 7 ms (13.01%) | Memory: 19.6 MB (6.71%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.cpp
+++ 1022-unique-paths-iii/unique-paths-iii.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    int check(vector<vector<int>>& grid,int an,int i,int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==-1 ){
+    int check(int i,int j,vector<vector<int>>& grid,set<pair<int,int>>&v,int c){
+
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]==-1|| v.count({i,j})){
             return 0;
         }
         if(grid[i][j]==2){
-            return an==0;
+            if(c==0)
+            return 1;
+            return 0;
         }
-        int temp=grid[i][j];
+        v.insert({i,j});
         grid[i][j]=-1;
-        int a=check(grid,an-1,i+1,j);
-        int b=check(grid,an-1,i-1,j);
-        int c=check(grid,an-1,i,j+1);
-        int d=check(grid,an-1,i,j-1);
-        grid[i][j]=temp;
-        return a+b+c+d;
+        int ans=0;
+        ans+=check(i+1,j,grid,v,c-1);
+        ans+=check(i-1,j,grid,v,c-1);
+        ans+=check(i,j+1,grid,v,c-1);
+        ans+=check(i,j-1,grid,v,c-1);
+        grid[i][j]=0;
+        v.erase({i,j});
+        return ans;
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
-        //so entire grid has to be 1/-1-
-        //map with 0 and 1
-        // unordered_map<int,int>>m;
-        int an=0;
-        int r=0,c=0;
+        int c=0;
+        int si,sj;
         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==0){
-                    // m[0].push_back({i,j});
-                    an++;
+                    c++;
                 }
                 if(grid[i][j]==1){
-                    r=i;
-                    c=j;
+                    si=i;
+                    sj=j;
                 }
-                
             }
-        } 
-        return check(grid,an+1,r,c);
+        }
+        set<pair<int,int>>v;
+        return check(si,sj,grid,v,c+1);
+        return -1;
     }
 };
