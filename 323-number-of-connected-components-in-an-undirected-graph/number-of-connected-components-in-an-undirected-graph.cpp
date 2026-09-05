ff484195eee833044ad3291908c68ef2c73decfd
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-09-05 23:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (55.91%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,28 +1,38 @@
+class DSU{
+    public:
+    vector<int>p;
+    int s=0;
+    DSU(int n){
+        p.resize(n+1);
+        for(int i=0;i<n;i++){
+            p[i]=i;
+        }
+        s=n;
+    }
+    int find(int node){
+        if(p[node]!=node){
+            return p[node]=find(p[node]);
+        }
+        return p[node];
+    }
+    bool Union(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv){
+            return false;
+        }
+        s--;
+        p[pu]=pv;
+        return true;
+    }
+};
 class Solution {
 public:
-    void check(unordered_map<int,vector<int>>&m,int node,unordered_set<int>&v){
-        if(v.count(node)){
-            return;
-        }
-        v.insert(node);
-        for(auto i:m[node]){
-            check(m,i,v);
-        }
-    }
     int countComponents(int n, vector<vector<int>>& edges) {
-        unordered_map<int,vector<int>>m;
+        DSU dsu(n);
         for(auto i:edges){
-            m[i[0]].push_back(i[1]);
-            m[i[1]].push_back(i[0]);
+            dsu.Union(i[0],i[1]);
         }
-        unordered_set<int>v;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(!v.count(i)){
-            check(m,i,v);
-            ans++;
-            }
-        }
-        return ans;
+        return dsu.s;
     }
 };
