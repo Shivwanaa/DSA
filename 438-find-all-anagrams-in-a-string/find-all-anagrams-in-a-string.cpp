6addc3bb660cf12f410d578d3720709544d96fdb
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-09-22 16:46:30 +0530

    Time: 32 ms (11.31%) | Memory: 15.7 MB (13.18%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -3,24 +3,34 @@ public:
     vector<int> findAnagrams(string s, string p) {
         vector<int>ans;
         unordered_map<char,int>m;
-        unordered_map<char,int>mtemp;
         for(auto i:p){
             m[i]++;
         }
-        int i=0;
-        for(int j=0;j<s.size();j++){
-            mtemp[s[j]]++;
-            if(mtemp==m){
-                ans.push_back(i);
-            }
+        unordered_map<char,int>mp;
+        int i=0,j=0;
+        for( j=0;j<s.size();j++){
+            mp[s[j]]++;
             if(j-i+1==p.size()){
-                mtemp[s[i]]--;
-                if(mtemp[s[i]]==0){
-                    mtemp.erase(s[i]);
+                if(mp==m){
+                ans.push_back(i);
+                }
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
                 }
                 i++;
             }
         }
+        if(j-i+1==p.size()){
+    if(mp==m){
+        ans.push_back(i);
+    }
+    mp[s[i]]--;
+    if(mp[s[i]]==0){
+        mp.erase(s[i]);
+        i++;
+    }
+}
         return ans;
     }
 };
