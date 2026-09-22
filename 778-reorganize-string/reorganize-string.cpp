103ec4af56b452fb7db10154549db45df95deb2b
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-09-22 18:17:15 +0530

    Time: 2 ms (26.45%) | Memory: 11.2 MB (5.19%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     string reorganizeString(string s) {
+        priority_queue<pair<int,char>>q;
         unordered_map<char,int>m;
         for(auto i:s){
             m[i]++;
         }
-        auto comp=[](pair<char,int>a,pair<char,int>b){
-            return a.second<b.second;
-        };
-        priority_queue<pair<char,int>,vector<pair<char,int>>,decltype(comp)>q(comp);
         for(auto i:m){
-            q.push({i.first,i.second});
+            q.push({i.second,i.first});
         }
         string ans="";
         while(q.size()){
-            auto[c,f]=q.top();
+            auto [freq,let]=q.top();
             q.pop();
-            if(ans.size() && c==ans[ans.size()-1]){
-                if(q.empty()){
+            if(ans.size() && let==ans.back()){
+                if(!q.size()){
                     return "";
                 }
-                auto [c1,f1]=q.top();
+                auto [f1,n]=q.top();
                 q.pop();
-                ans=ans+c1;
-                if(f1-1>0){
-                    q.push({c1,f1-1});
+                ans=ans+n;
+                f1--;
+                if(f1){
+                    q.push({f1,n});
                 }
+                q.push({freq,let});
             }
             else{
-                ans=ans+c;
-                f--;
-            }
-            if(f){
-                q.push({c,f});
+                ans=ans+let;
+                freq--;
+                if(freq){
+                    q.push({freq,let});
+                }
             }
         }
-
-        if(ans.size()==s.size())
         return ans;
-        return "";
     }
 };
