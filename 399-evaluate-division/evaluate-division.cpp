fc6eb0cad827cf5810262efcb4a103b3681d0840
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2026-09-23 19:44:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (14.52%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    double check(string n1,string n2,unordered_map<string,vector<pair<string,double>>>&m,unordered_set<string>v){
-        if(n1==n2){
+vector<double>ans;
+    double check(string start,string end,unordered_map<string,vector<pair<string,double>>>&m,unordered_set<string>&s){
+        if(start==end){
             return 1.0;
         }
-        if(v.count(n1)){
-            return -1.0;
-        }
-        v.insert(n1);
-        for(auto i:m[n1]){
-            if(!v.count(i.first)){
-            double result = check(i.first, n2, m, v);
-            if(result != -1.0) {
-                return i.second * result;
-            }
+        s.insert(start);
+        for(auto i:m[start]){
+            if(!s.count(i.first)){
+                double temp = check(i.first, end, m, s);
+                if(temp!=-1) {
+                    return i.second*temp;
+                }
             }
         }
-        return -1;
+        return -1.0;
     }
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         unordered_map<string,vector<pair<string,double>>>m;
         for(int i=0;i<equations.size();i++){
-            m[equations[i][0]].push_back({equations[i][1],values[i]});
             m[equations[i][1]].push_back({equations[i][0],1.0/values[i]});
+            m[equations[i][0]].push_back({equations[i][1],values[i]});
         }
-        vector<double> ans;
-
+        
         for(auto i:queries){
-            if(!m.count(i[0])||!m.count(i[1])){
+            if(!m.count(i[0]) || !m.count(i[1])) {
                 ans.push_back(-1.0);
+                continue;
             }
-            else{
-            unordered_set<string>v;
-            ans.push_back(check(i[0],i[1],m,v));
-            }
+            unordered_set<string>s;
+            ans.push_back(check(i[0],i[1],m,s));
         }
         return ans;
     }
